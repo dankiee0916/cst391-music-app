ee1bc8d1dd038e82d6c2a2f7214decef7c341028
Cisco <ohdankiee@gmail.com>
2025-10-20 23:27:06 -0700

    Local Connection

--- src/app/api/db-check/route.ts
+++ src/app/api/db-check/route.ts
@@ -1,19 +1,21 @@
 // api/db-check/route.ts
-import { getPool } from "@/lib/db";
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import { getPool } from '@/lib/db';
 
 const environment = process.env.NODE_ENV;
-const dbUrl = process.env.DATABASE_URL;
+const dbUrl = process.env.POSTGRES_URL ?? process.env.DATABASE_URL;
 
 export async function GET() {
   try {
     const db = getPool();
-    const { rows } = await db.query('select now() as now');
-    return NextResponse.json({ time: rows[0], message: 'Database check OK!' });
-  } catch (err: any) {
-    return NextResponse.json(
-      { error: 'Database connection failed', details: err.message },
-      { status: 500 }
-    );
+    let { rows } = await db.query('select now() as now');
+    const now = rows[0]?.now;
+    ({ rows } = await db.query('SELECT artist FROM albums LIMIT 1'));
+    const artist = rows[0]?.artist;   
+    return NextResponse.json({ time: now, artist: artist, message: `Francisco's Database connection successful. Running in ${environment}. DATABASE_URL: ${dbUrl}` }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ error: 'Database connection failed',
+       details: (err as Error).message, message: `Sparks Database connection failed. Running in ${environment}. DATABASE_URL: ${dbUrl}` }, { status: 500 });
   }
+
 }
